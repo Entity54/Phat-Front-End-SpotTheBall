b588a51b58f9b2875ab02378f734c105e96264c5
Entity54 <daniel@entity54.com>
2023-04-21 20:45:41 +0200

    Live Comp Tables

--- src/jsx/components/Screens/LiveJobs.js
+++ src/jsx/components/Screens/LiveJobs.js
@@ -2,6 +2,8 @@ import React,{Fragment,useContext, useState, useEffect} from 'react';
 import loadable from "@loadable/component";
 import pMinDelay from "p-min-delay";
 import { ThemeContext } from "../../../context/ThemeContext";
+import currentcomptitle from "../../../images/currentcomptitle.png";
+import halloffametitle from "../../../images/halloffametitle.png";
 
 import {
   Row,
@@ -113,295 +115,108 @@ const LiveJobs = ({ astar_api,  blockHeader }) => {
 	return(
 		<Fragment>
 			<div className="row">
-			<Col lg={12}>
-				<Card className="bg-gradient-1 mb-1">
-					<Card.Header>
-					<p className="fs-24 mb-0" style={{color:"#AEAEAE"}}>Pending Jobs</p>
-					</Card.Header>
-					<Card.Body className="fs-24 text-center">
-					<Table responsive bordered className="verticle-middle table-hover"style={{border:"solid"}}>
-						<thead>
-						<tr className="text-center" style={{border:"solid"}}>
-							<th scope="col" style={{color:"#AEAEAE"}}>Id</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Request</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Type</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>First Payment Date</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Token</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Denominated In USD</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Total Amount</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payee</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Progress</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Status</th>
-						</tr>
-						</thead>
-						<tbody className="fs-16">
+				<Col lg={3}>
+					<Card className="bg-gradient-1 mb-1">
+						<Card.Header>
+							{/* <p className="fs-24 mb-0" style={{color:"#AEAEAE"}}>Current Competition</p> */}
+							<img alt="images" height={60} src={currentcomptitle} ></img>
+						</Card.Header>
+						<Card.Body className="fs-24 text-center">
+							<div className="row mb-4">
+								<div className="form-group col-md-4 d-flex align-items-center p-0"style={{backgroundColor:""}}>
+									<button type="submit" className="btn btn-primary text-center mx-4"style={{marginTop:"30px", width:"100%"}}>
+										Start
+									</button>
+								</div>
+								<div className="col-md-4 text-white fs-18"style={{backgroundColor:""}}>
+									<label>Start Time</label>
+									<input
+										type="textarea"
+										className="form-control fs-18 text-center"
+										placeholder=""
+										// value={}
+										// onChange={(event) => (event.target.value)}
+									/>
+								</div>
+								<div className="col-md-4 text-white fs-18"style={{backgroundColor:""}}>
+									<label>End Time</label>
+									<input
+										type="textarea"
+										className="form-control fs-18 text-center"
+										placeholder=""
+										// value={}
+										// onChange={(event) => (event.target.value)}
+									/>
+								</div>
+							</div>
+							<Table responsive bordered className="verticle-middle table-hover"style={{border:"solid"}}>
+								<thead>
+									<tr className="text-center" style={{border:"solid"}}>
+										<th scope="col" style={{color:"#AEAEAE"}}>Owner</th>
+										<th scope="col" style={{color:"#AEAEAE"}}>Ticket Coordinates</th>
+										<th scope="col" style={{color:"#AEAEAE"}}>Distance From Target</th>
+									</tr>
+								</thead>
+								<tbody className="fs-16 text-center">
+									{pendingJobsArray.map((data,index)=>(	
+									<tr  key={index} >
+										<td>{data.id}</td>
+										<td>{data.title}</td>
+										<td>{data.paymentType}</td>
+									</tr>
+								))}
+								</tbody>
+							</Table>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col lg={9}>
+					<Card className="bg-gradient-1 mb-1">
+						<Card.Header>
+						<img alt="images" height={45} src={halloffametitle} ></img>
+						</Card.Header>
+						<Card.Body>
+							<Table responsive bordered className="verticle-middle table-hover"style={{border:"solid"}}>
+								<thead>
+								<tr className="text-center" style={{border:"solid"}}>
+									<th scope="col" style={{color:"#AEAEAE"}}>Owner</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Ticket Coordinates</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Distance From Target</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Prize Money</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Timestamp</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Competition Num</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Start Time</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>End Time</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Num Of Tickets</th>
+									<th scope="col" style={{color:"#AEAEAE"}}>Num Of Players</th>
+								</tr>
+								</thead>
+								<tbody className="fs-16 text-center">
+									{pendingJobsArray.map((data,index)=>(	
+										<tr  key={index} >
+											<td>{data.id}</td>
+											<td>{data.title}</td>
+											<td>{data.paymentType}</td>
+											<td>{data.first_payment_date}</td>
+											<td>{data.paymentToken}</td>
 
-						{openJobsArray.map((data,index)=>(	
-							<tr  key={index} >
-								<td>{data.id}</td>
-								<td>{data.title}</td>
-								<td>{data.paymentType}</td>
-								<td>{data.first_payment_date}</td>
-								<td>{data.paymentToken}</td>
-
-								<td>{data.denomnatedinUSD? "Yes" : "No"}</td>
-								<td>{data.amount}</td>
-								<td>{data.payee}</td>
-								<td>
-								<ProgressBar now={0} variant="warning" />
-								</td>
-								<td>
-								<Badge variant="warning light">0%</Badge>
-								</td>
-							</tr>
-						))}
-
-
-
-						{/* <tr >
-							<td>Berlin Polkadot Meetup</td>
-							<td>Single Payment</td>
-							<td>Apr 19,2023</td>
-							<td>DOT</td>
-							<td>No</td>
-							<td>250</td>
-							<td>
-							<ProgressBar now={0} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">0%</Badge>
-							</td>
-						</tr> */}
-						{/* <tr>
-							<td>ETH Denver Hackathon Prizes</td>
-							<td>Single Payment</td>
-							<td>Apr 20,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>25000</td>
-							<td>
-							<ProgressBar now={0} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">0%</Badge>
-							</td>
-						</tr> */}
-						{/* <tr>
-							<td>Monthly Article Translations</td>
-							<td>Interval Payment</td>
-							<td>Apr 21,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>100</td>
-							<td>
-							<ProgressBar now={0} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">0%</Badge>
-							</td>
-						</tr> */}
-
-						</tbody>
-					</Table>
-					</Card.Body>
-				</Card>
-        	</Col>
-		</div>
-
-		<div className="row">
-			<Col lg={12}>
-				<Card className="bg-gradient-1 mb-1">
-					<Card.Header>
-					<p className="fs-24 mb-0" style={{color:"#AEAEAE"}}>Live Jobs</p>
-					</Card.Header>
-					<Card.Body>
-					<Table responsive bordered className="verticle-middle table-hover"style={{border:"solid"}}>
-						<thead>
-						<tr className="text-center" style={{border:"solid"}}>
-							<th scope="col" style={{color:"#AEAEAE"}}>Id</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Request</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Type</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>First Payment Date</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Token</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Denominated In USD</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Total Amount</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payee</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Progress</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Status</th>
-						</tr>
-						</thead>
-						<tbody className="fs-16 text-center">
-
-						{pendingJobsArray.map((data,index)=>(	
-							<tr  key={index} >
-								<td>{data.id}</td>
-								<td>{data.title}</td>
-								<td>{data.paymentType}</td>
-								<td>{data.first_payment_date}</td>
-								<td>{data.paymentToken}</td>
-
-								<td>{data.denomnatedinUSD? "Yes" : "No"}</td>
-								<td>{data.amount}</td>
-								<td>{data.payee}</td>
-								<td>
-								<ProgressBar now={data.progress} variant="warning" />
-								</td>
-								<td>
-								<Badge variant="warning light">{data.progress}%</Badge>
-								</td>
-							</tr>
-						))}
-						
-						
-						{/* <tr>
-							<td>Monthly Ambassador Program</td>
-							<td>Interval Payment</td>
-							<td>Feb 25,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>500</td>
-							<td>
-							<ProgressBar now={8.5} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">8.5%</Badge>
-							</td>
-						</tr>
-						<tr>
-							<td>Quarterly Node Management</td>
-							<td>Interval Payment</td>
-							<td>Feb 26,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>3450</td>
-							<td>
-							<ProgressBar now={25} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">25%</Badge>
-							</td>
-						</tr>
-						<tr>
-							<td>Monthly Core Dev Payroll</td>
-							<td>Interval Payment</td>
-							<td>Dec 31,2022</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>34500</td>
-							<td>
-							<ProgressBar now={17} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">17%</Badge>
-							</td>
-						</tr> */}
-
-
-
-						</tbody>
-						</Table>
-					</Card.Body>
-				</Card>
-        	</Col>
-		</div>
-
-		<div className="row">
-			<Col lg={12}>
-				<Card className="bg-gradient-1 mb-1">
-					<Card.Header>
-					<p className="fs-24 mb-0" style={{color:"#AEAEAE"}}>Completed Jobs</p>
-					</Card.Header>
-					<Card.Body>
-					<Table responsive bordered className="verticle-middle table-hover"style={{border:"solid"}}>
-						<thead>
-						<tr className="text-center" style={{border:"solid"}}>
-							<th scope="col" style={{color:"#AEAEAE"}}>Id</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Request</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Type</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>First Payment Date</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payment Token</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Denominated In USD</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Total Amount</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Payee</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Progress</th>
-							<th scope="col" style={{color:"#AEAEAE"}}>Status</th>
-						</tr>
-						</thead>
-						<tbody className="fs-16 text-center">
-
-						{completedJobsArray.map((data,index)=>(	
-							<tr  key={index} >
-								<td>{data.id}</td>
-								<td>{data.title}</td>
-								<td>{data.paymentType}</td>
-								<td>{data.first_payment_date}</td>
-								<td>{data.paymentToken}</td>
-
-								<td>{data.denomnatedinUSD? "Yes" : "No"}</td>
-								<td>{data.amount}</td>
-								<td>{data.payee}</td>
-								<td>
-								<ProgressBar now={data.progress} variant="warning" />
-								</td>
-								<td>
-								<Badge variant="warning light">{data.progress}%</Badge>
-								</td>
-							</tr>
-						))}
-
- 
-						{/* <tr>
-							<td>Paris Substrate Meetup</td>
-							<td>Single Payment</td>
-							<td>Feb 10,2023</td>
-							<td>DOT</td>
-							<td>No</td>
-							<td>180</td>
-							<td>
-							<ProgressBar now={100} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">100%</Badge>
-							</td>
-						</tr>
-						<tr>
-							<td>Chinese Article Translation</td>
-							<td>Single Payment</td>
-							<td>Feb 12,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>85</td>
-							<td>
-							<ProgressBar now={100} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">100%</Badge>
-							</td>
-						</tr>
-						<tr>
-							<td>Charity Bike Ride Sponsorship</td>
-							<td>Single Payment</td>
-							<td>Feb 13,2023</td>
-							<td>DOT</td>
-							<td>Yes</td>
-							<td>250</td>
-							<td>
-							<ProgressBar now={100} variant="warning" />
-							</td>
-							<td>
-							<Badge variant="warning light">100%</Badge>
-							</td>
-						</tr> */}
-						
-						</tbody>
-						</Table>
-					</Card.Body>
-				</Card>
-        	</Col>
-		</div>
-
-
-
+											<td>{data.denomnatedinUSD? "Yes" : "No"}</td>
+											<td>{data.amount}</td>
+											<td>{data.payee}</td>
+											<td>
+											<ProgressBar now={data.progress} variant="warning" />
+											</td>
+											<td>
+											<Badge variant="warning light">{data.progress}%</Badge>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</Table>
+						</Card.Body>
+					</Card>
+				</Col>
+			</div>
 		</Fragment>
 	)
 
